f8ca89db17d9cab1a1a200bd3e50e5ac061bc963
Shrasti <shrastipachauri3@gmail.com>
2024-04-11 18:08:24 +0530

    test: added test cases for user routes and service

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserService } from "./users.service";
 import { UserController } from "./users.controller";
+import { IFilterResponse } from "src/common/interfaces/response";
 
 describe("UserController", () => {
   let controller: UserController;
@@ -39,19 +40,75 @@ describe("UserController", () => {
 
       expect(result).toEqual(mockUser);
     });
+
+    it("should throw an error when cat ID does not exist", async () => {
+      const invalidCatId = 8;
+      const response: IFilterResponse = {
+        timestamp: expect.any(String),
+        path: `/user/favorite-cat/${invalidCatId}`,
+        message: "Cat not found",
+        statusCode: 404,
+      };
+
+      jest.spyOn(userService, "markCatAsFavorite").mockRejectedValue(response);
+
+      const request = { user: { id: 1, name: "John", roles: ["user"] } };
+
+      await expect(
+        controller.markCatAsFavorite(invalidCatId, request)
+      ).rejects.toEqual(response);
+
+      expect(userService.markCatAsFavorite).toHaveBeenCalledWith(
+        request.user.id,
+        invalidCatId
+      );
+    });
+
+    it("should throw an error when cat ID type is not valid", async () => {
+      const invalidCatId: any = "22";
+      const response: IFilterResponse = {
+        timestamp: expect.any(String),
+        path: `/user/favorite-cat/${invalidCatId}`,
+        message: "Validation failed",
+        statusCode: 400,
+      };
+
+      jest.spyOn(userService, "markCatAsFavorite").mockRejectedValue(response);
+
+      const request = { user: { id: 1, name: "John", roles: ["user"] } };
+
+      await expect(
+        controller.markCatAsFavorite(invalidCatId, request)
+      ).rejects.toEqual(response);
+
+      expect(userService.markCatAsFavorite).toHaveBeenCalledWith(
+        request.user.id,
+        invalidCatId
+      );
+    });
   });
 
   describe("getFavoriteCats", () => {
     it("should return favorite cats for the user", async () => {
-      const mockUser = [{ id: 1, name: "cat", breed: "testBreed", age: 10 }];
-      jest
-        .spyOn(userService, "getUserFavoriteCats")
-        .mockResolvedValue(mockUser);
+      const mockCat = [{ id: 1, name: "cat", breed: "testBreed", age: 10 }];
+      jest.spyOn(userService, "getUserFavoriteCats").mockResolvedValue(mockCat);
 
       const request = { user: { id: 1, name: "John", roles: ["user"] } };
       const result = await controller.getFavoriteCats(request);
 
-      expect(result).toEqual(mockUser);
+      expect(result).toEqual(mockCat);
+    });
+
+    it("should return an empty array when user not have favorite cats", async () => {
+      const request = { user: { id: 1, name: "John", roles: ["user"] } };
+      const mockUser = { id: 1, favorites: [] };
+      jest
+        .spyOn(userService, "getUserFavoriteCats")
+        .mockResolvedValue(mockUser.favorites);
+
+      const result = await controller.getFavoriteCats(request);
+
+      expect(result).toEqual([]);
     });
   });
 });
