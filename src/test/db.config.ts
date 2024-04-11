0d47ed1c28641bc598a655101ce87dbc54842587
pragati <pnayak@deqode.com>
2024-04-11 13:26:14 +0530

    docker build for production

--- src/test/db.config.ts
+++ src/test/db.config.ts
@@ -3,10 +3,10 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 const testDbConfig: TypeOrmModuleOptions = {
   type: "postgres",
   host: "localhost",
-  port: 54325,
-  username: "test_user",
-  password: "test_password",
-  database: "test_database",
+  port: 5434,
+  username: "user",
+  password: "password",
+  database: "tundrax_test_db",
   entities: ["./**/*.entity.ts"],
   synchronize: true,
 };
